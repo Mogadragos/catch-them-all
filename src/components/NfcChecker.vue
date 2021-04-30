94220d2ce31a78ac2769f39153c6b0a96e30435c
Clément <clementgomez99@gmail.com>
2021-04-30 19:42:34 +0200

    NFC read n check

--- src/components/NfcChecker.vue
+++ src/components/NfcChecker.vue
@@ -7,6 +7,34 @@
 <script>
 export default {
   emits: ["NFCChecked"],
+  methods :{
+    check : async function(){
+      if ("NDEFReader" in window) {
+        console.log("NFC présent");
+      }else{
+        console.log("NFC indisponible");
+      }
+    },
+    Read : async function(){
+      if ("NDEFReader" in window) {
+        const ndef = new NDEFReader();// obj for interaction with NFC
+         return new Promise((resolve, reject) => {
+          const ctlr = new AbortController();
+          ctlr.signal.onabort = reject;
+          ndef.addEventListener("reading", event => {
+            ctlr.abort();
+            resolve(event);
+          }, { once: true });
+          ndef.scan({ signal: ctlr.signal }).catch(err => reject(err));
+        });
+      }
+    },
+    readLauch : function(){
+      read().then(({ serialNumber }) => {
+        console.log(serialNumber);
+      });
+    }
+  },
   mounted() {
     this.$emit("NFCChecked", "NDEFReader" in window);
   },
