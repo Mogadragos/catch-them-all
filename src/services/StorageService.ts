493b194853dc1f4f6ba6533a0c848893ba85e4f7
Mogador <jcaillaud17170@gmail.com>
2021-05-01 13:39:52 +0200

    Convert to TypeScript

--- src/services/StorageService.ts
+++ src/services/StorageService.ts
@@ -1,6 +1,18 @@
 import data from "../assets/activities.json";
 
+type Activity = {
+    start_chip_id: string,
+    activity_id: string,
+    name : string,
+    chips: Object
+  };
+
 class StorageService {
+    activities: Object;
+    activityIsSet: boolean;
+    activity: Activity;
+    active_chips: Array<Object>;
+
     constructor() {
         this.activities = data;
         this.activityIsSet = false;
@@ -10,7 +22,7 @@ class StorageService {
      * Récupération et mise en place des données de l'activité
      * @param {string} startChipId 
      */
-    setActivity(start_chip_id) {
+    setActivity(start_chip_id: string) {
 
         this.activity = this.activities[start_chip_id];
 
@@ -29,7 +41,7 @@ class StorageService {
      * Découverte d'une nouvelle puce
      * @param {string} chip_id 
      */
-    discoverChip(chip_id) {
+    discoverChip(chip_id: string) {
         if(this.activityIsSet) {
             console.log(localStorage.active_chips);
             localStorage.active_chips.push(this.activity.chips[chip_id]);
