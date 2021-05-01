194daf84ed68bab09f674700f74d6f8df80c0084
Mogador <jcaillaud17170@gmail.com>
2021-05-01 13:26:21 +0200

    Refactor files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <app-content v-if="NfcChecked" :NfcActive="NfcActive"></app-content>
+  <header-help></header-help>
+  <app-content v-if="NfcChecked && NfcActive"></app-content>
+  <section v-else-if="NfcChecked">No NFC</section>
+  <section>
+    <modal
+      :show="showSettingsModal"
+      :zIndex="150"
+      @close="showSettingsModal = false"
+    >
+    </modal>
+  </section>
 </template>
 
 <script>
+import HeaderHelp from "./components/HeaderHelp";
 import AppContent from "./components/AppContent";
+import Modal from "./components/Modal";
 import NFCService from "./services/NFCService.ts";
 
 export default {
   name: "App",
   components: {
+    HeaderHelp,
     AppContent,
+    Modal,
   },
   data() {
     return {
--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,41 +1,17 @@
 <template>
-  <header-help></header-help>
   <section id="main-content">
-    <section v-if="NfcActive">
-      NFC Ready
-      <launch-button></launch-button>
-    </section>
-    <section v-else>No NFC</section>
-  </section>
-  <section>
-    <modal
-      :show="showSettingsModal"
-      :zIndex="150"
-      @close="showSettingsModal = false"
-    >
-    </modal>
+    NFC Ready
+    <launch-button></launch-button>
   </section>
 </template>
 
 <script>
-import Modal from "./Modal";
-import HeaderHelp from "./HeaderHelp";
 import LaunchButton from "./LaunchButton.vue";
 //import NFCService from "../services/NFCService.ts";
 
 export default {
   components: {
-    Modal,
-    HeaderHelp,
     LaunchButton,
-  },
-  props: {
-    NfcActive: Boolean,
-  },
-  data() {
-    return {
-      showSettingsModal: false,
-    };
   },
 };
 </script>
